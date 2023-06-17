23ea99ddf27afe01eba0446e267318f6e437d3a7
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-06-17 00:02:47 -0700

    a thing

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,11 +4,26 @@ const resolvers = {
     Qeury: {
        user: async () => {
         const params = _id ? {_id} : {};
-        return User.find(params);
+        return User.findOne(params);
+       },
+       users: async () => {
+        return User.find();
        }
     },
     Mutation: {
-
+        login: async () => {
+            const params = {$or: [{username: body.username}, {email: body.email }]};
+            return User.findOne(params);
+        },
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+        saveBook: async () => {
+            
+        }
+        
     },
 };
 
